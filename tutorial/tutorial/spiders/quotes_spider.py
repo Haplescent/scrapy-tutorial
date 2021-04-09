1f0ea43081e52f46b3e4045fb82dc87099fc2ab3
John Merritt <johnmerritt95@gmail.com>
2021-04-08 19:44:59 -0700

    motified spider to take all text

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,5 +1,23 @@
 import scrapy
 
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -10,6 +28,7 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        print(text_from_html(response.body))
+        filename = f'quotes-{page}.txt'
+        with open(filename, 'w') as f:
+            f.write(text_from_html(response.body))
